7a4e64621f27278fa0b00176709f63d498ee92ab
AnuranDutta <anuran_201700244@smit.smu.edu.in>
2020-06-29 23:36:35 +0530

    Header-Footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,9 +8,9 @@ function Footer(props) {
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
                     <ul className="list-unstyled">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Menu</a></li>
                         <li><a href="contactus.html">Contact</a></li>
                     </ul>
                 </div>
